f4bd7a7fffe287af4409f05ece9aad5615b2d884
Inês Torres <inestsilva2001@hotmail.com>
2026-05-25 12:34:20 +0100

    Created tasks component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,18 +2,24 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header';
 import { User } from './user/user';
+import { Tasks } from './tasks/tasks';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, User],
+  imports: [RouterOutlet, HeaderComponent, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string) {
-    console.log('Selected user with id ' + id);
+    this.selectedUserId = id;
   }
 }
